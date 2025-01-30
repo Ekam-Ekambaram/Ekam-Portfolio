aa0d1dcdde7cc7b77df65fba34b694f6c672306d
Ekam-Ekambaram <ekmgbekambaram@gmail.com>
2025-01-30 16:32:10 +0530

    first commit

---
+++ My-Portfolio/src/data/headerData.js
@@ -0,0 +1,11 @@
+import resume from '../assets/pdf/resume.pdf'
+import profile_image from '../assets/png/profile_image.jpg'
+
+
+export const headerData = {
+    name: 'Golla Ekambaram',
+    title: "Full Stack Developer",
+    desciption:"I am proficient in HTML, CSS, JavaScript,Python, Sqlite and React.js Passionate about clean code and user-friendly design, I value teamwork, innovation, and integrity. I am available for full-time or Internship opportunities",
+    image: profile_image,
+    resumePdf: resume
+}
---
+++ My-Portfolio/src/data/projectsData.js
@@ -0,0 +1,117 @@
+import one from '../assets/svg/projects/one.svg'
+import two from '../assets/svg/projects/two.svg'
+import three from '../assets/svg/projects/three.svg'
+import four from '../assets/svg/projects/four.svg'
+import five from '../assets/svg/projects/five.svg'
+import six from '../assets/svg/projects/six.svg'
+import seven from '../assets/svg/projects/seven.svg'
+import eight from '../assets/svg/projects/eight.svg'
+import one_solutions_logo from "../assets/png/one_solutions_logo.png"
+
+export const projectsData = [
+    {
+        id: 1,
+        projectName: 'Nxt Trendz ( ECommerce Clone - Amazon, Flipkart)',
+        projectDesc: 'Implemented Nxt Trendz application which is a clone for ECommerce applications like Amazon, Flipkart where users can login and can see list of products with search, filters, sort by, etc.',
+        tags: ['React JS', 'JS', 'CSS', 'Bootstrap', 'Routing', 'REST API Calls', 'Local Storage', 'JWT Token', 'Authorization', 'Authentication'],
+    //    code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+        demo: 'ekamNxtTrenz.ccbp.tech',
+        image: one
+    },
+    {
+        id: 2,
+        projectName: 'One Solutions (Job Notifications)',
+        projectDesc: 'Developed a full-stack web application for posting and managing job notifications, enabling job seekers to search and apply for jobs.',
+        tags: [' HTML', 'CSS', 'Bootstrap, Reactjs, Nodejs'],
+      //  code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+        demo: 'https://onesolutions.onrender.com/',
+        image: one_solutions_logo
+    },{
+        id: 3,
+        projectName: 'One Solutions (Admin Panel)',
+        projectDesc: 'Developed a secure admin panel for managing job notifications, with authentication and authorization for registered admins.• Implemented login and registration using POST HTTP API calls.',
+        tags: ['Html5, Css3, React JS', 'JS', 'CSS', 'Bootstrap', 'Routing', 'REST API Calls', 'Local Storage', 'JWT Token', 'Authorization', 'Authentication'],
+      //  code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+        demo: 'https://onesolutions-admin.onrender.com/login/',
+        image: one_solutions_logo
+    },
+    {
+        id: 4,
+        projectName: 'Tourism Website',
+        projectDesc: 'Devised an immersive web experience for users to discover top travel destinations through images, videos, and descriptions.',
+        tags: [' HTML', 'CSS', 'Bootstrap'],
+      //  code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+        demo: 'ekamtourism.ccbp.tech',
+        image: two
+    },
+    {
+        id: 5,
+        projectName: 'Weather App',
+        projectDesc: 'Weather forecast systems and applications predict weather conditions based on multiple parameters.',
+        tags: ['Html', 'CSS', 'JS', 'Reactjs'],
+     //   code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+        demo: 'https://weatherekam.ccbp.tech/',
+        image: three
+    },
+    {
+        id: 6,
+        projectName: 'Food Munch',
+        projectDesc: 'A fully responsive website that offers a comprehensive list of food items and detailed information about each item.',
+        tags: ['HTML', 'CSS', 'Bootstrap'],
+     //   code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+        demo: 'ekamfoodmunch.ccbp.tech',
+        image: four
+    },
+    {
+        id: 7,
+        projectName: 'E-Commerce App',
+        projectDesc: 'A Simple E-commerce application',
+        tags: ['React JS', 'JS', 'CSS', 'Bootstrap', 'Routing', 'REST API Calls', 'Local Storage', 'JWT Token', 'Authorization', 'Authentication'],
+      //  code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+      demo: 'ekamNxtTrenz.ccbp.tech',
+      image: five
+    },
+    {
+        id: 8,
+        projectName: 'Wikipedia Search Application ',
+        projectDesc: 'A fully responsive website that offers a comprehensive list of food items and detailed information about each item.',
+        tags: [' HTML', 'CSS', 'Bootstrap'],
+      //  code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+        demo: 'ekamfoodmunch.ccbp.tech',
+        image: six
+    },
+    {
+        id: 9,
+        projectName: 'Tourism Website',
+        projectDesc: 'Devised an immersive web experience for users to discover top travel destinations through images, videos, and descriptions.',
+        tags: ['HTML', 'CSS', 'Bootstrap'],
+      //  code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+        demo: 'ekamtourism.ccbp.tech',
+        image: seven
+    },
+    {
+        id: 10,
+        projectName: 'Insta Share (Instagram Clone)',
+        projectDesc: ' Implemented responsive Streaming Application like Instagram Clone where users can log in and see a list of stories and posts, user can also search posts with post caption, user can be able to like and dislike the post and also view the user specific posts.',
+        tags: [' HTML', 'CSS', 'JavaScript', 'React JS', 'Bootstrap', 'React Slick', 'Figma', 'client storage'],
+     //   code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+        demo: 'https://ekaminsta.ccbp.tech/login',
+        image: eight
+    },
+]
+
+
+// Do not remove any fields.
+// Leave it blank instead as shown below
+
+/* 
+{
+    id: 1,
+    projectName: 'Car Pooling System',
+    projectDesc: '',
+    tags: ['Flutter', 'React'],
+    code: '',
+    demo: '',
+    image: ''
+}, 
+*/
